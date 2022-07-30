49c3dfd50d83bbd8597314e3e034fe79fa730352
FoxT3c <quentin.abel-marceau@epita.fr>
2022-07-30 22:00:31 +0200

    feat(repos): added repos

---
+++ src/lib/components/RepoCard.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    export let title: string;
+    export let description: string;
+    export let url: string;
+    export let tags: string[];
+    export let stars: string;
+    export let language: string;
+</script>
+
+<a class="flex flex-row relative hover:bg-slate-600 rounded cursor-pointer p-2" href={url} target="_blank">
+    <div class="flex flex-col">
+        <p class="text-lg font-semibold">{title}</p>
+        <p>{description}</p>
+    </div>
+    {#if language}
+        <div class="absolute right-1 top-1">
+            {language}
+        </div>
+    {/if}
+    <div class="absolute right-1 bottom-1">
+        <p>
+            {#each tags as tag}
+                <div class="badge badge-primary">
+                    {tag}
+                </div>
+            {/each}
+        {stars} <i class="fa-solid fa-star text-warning"></i></p>
+    </div>
+</a>
